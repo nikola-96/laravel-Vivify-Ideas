3f1e6403eca81f703617f5363779c3d69ea6599d
Nikola <nikolaviki@homail.com>
2020-09-08 08:38:37 +0200

    #LAGJ-10 adding users table, and adding new column to users table

---
+++ database/migrations/2020_09_08_063050_add_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('last_name');
+            $table->string('company');
+            $table->string('country');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_name');
+            $table->dropColumn('company');
+            $table->dropColumn('country');
+        });
+    }
+}
